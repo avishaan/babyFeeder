6afd8625121f5097e498628e8d572454b380b5a1
Avishaan <avishaan@codeHatcher.com>
2015-08-04 02:37:38 -0700

    change button color on press

--- breastFeed WatchKit Extension/InterfaceController.swift
+++ breastFeed WatchKit Extension/InterfaceController.swift
@@ -11,21 +11,53 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
-    override func awakeWithContext(context: AnyObject?) {
-        super.awakeWithContext(context)
-        
-        // Configure interface objects here.
+  
+  @IBOutlet weak var leftTimer: WKInterfaceTimer!
+  @IBOutlet weak var rightTimer: WKInterfaceTimer!
+  @IBOutlet weak var totalTimer: WKInterfaceTimer!
+  @IBOutlet weak var leftButton: WKInterfaceButton!
+  @IBOutlet weak var rightButton: WKInterfaceButton!
+  
+  var buttonState = (left: false, right: false)
+  
+  override func awakeWithContext(context: AnyObject?) {
+    super.awakeWithContext(context)
+    
+    // Configure interface objects here.
+  }
+  
+  override func willActivate() {
+    // This method is called when watch view controller is about to be visible to user
+    super.willActivate()
+  }
+  
+  override func didDeactivate() {
+    // This method is called when watch view controller is no longer visible
+    super.didDeactivate()
+  }
+  @IBAction func onRightButtonTap() {
+    // is the left button on
+    if buttonState.left {
+      // no matter what, disable the left button if it is enabled
+      buttonState.left = false
+      leftButton.setBackgroundColor(UIColor.clearColor())
     }
-
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
+    
+    // is this, the right button, on?
+    if buttonState.right {
+      // if right button is enabled, disable
+      buttonState.right = false
+      rightButton.setBackgroundColor(UIColor.blackColor())
+    } else {
+      // if right button is disabled, enable
+      buttonState.right = true
+      rightButton.setBackgroundColor(UIColor.greenColor())
+      
     }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
-
+  }
+  
+  @IBAction func onLeftButtonTap() {
+    
+    
+  }
 }
