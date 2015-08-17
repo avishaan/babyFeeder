f205b5bb927ecd91d8d3938a09f9bf75c3503ee0
Avishaan <avishaan@codeHatcher.com>
2015-08-16 18:58:36 -0700

    helper class to keep track of lastfeed duration

---
+++ breastFeed WatchKit Extension/LastDuration.swift
@@ -0,0 +1,37 @@
+//
+//  LastDuration.swift
+//  breastFeed
+//
+//  Created by Brown Magic on 8/16/15.
+//  Copyright (c) 2015 codeHatcher. All rights reserved.
+//
+
+import Foundation
+
+class LastDuration {
+  var durationInterval:NSTimeInterval!
+  let timeFormatter = NSDateComponentsFormatter()
+  
+  init(startDuration interval:Double = 0.0) {
+    self.durationInterval = NSTimeInterval(interval)
+    
+    // just need the minutes back
+    self.timeFormatter.unitsStyle = .Short
+  }
+  
+  var text: String {
+    get {
+      let string = self.timeFormatter.stringFromTimeInterval(self.durationInterval)
+      return string!
+    }
+  }
+  var interval: Double {
+    set {
+      self.durationInterval = NSTimeInterval(newValue)
+    }
+    get {
+      
+      return self.durationInterval!
+    }
+  }
+}
