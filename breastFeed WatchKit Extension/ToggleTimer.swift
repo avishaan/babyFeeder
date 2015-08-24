76aaeea2edb9f12d62eaa6736545096ed980f265
Avishaan <avishaan@codeHatcher.com>
2015-08-24 04:34:39 -0700

    have timers restore and persist state

--- breastFeed WatchKit Extension/ToggleTimer.swift
+++ breastFeed WatchKit Extension/ToggleTimer.swift
@@ -14,17 +14,18 @@ class ToggleTimer {
   
   var lastOn:Side = .None
   var currentOn:Side = .None
+  var userDefaults = NSUserDefaults.standardUserDefaults()
   
-  enum State {
-    case Active
-    case LastOn
-    case None
+  enum State:String {
+    case Active = "Active"
+    case LastOn = "LastOn"
+    case None = "None"
   }
   
-  enum Side {
-    case Left
-    case Right
-    case None
+  enum Side:String {
+    case Left = "Left"
+    case Right = "Right"
+    case None = "None"
   }
   
   init() {
@@ -69,7 +70,55 @@ class ToggleTimer {
   
   func restoreState() {
     println("Restore State")
+    // restore button state
+    if let currentOn:String = userDefaults.objectForKey("currentOn") as? String {
+      switch currentOn {
+      case "Left":
+        self.currentOn = .Left
+      case "Right":
+        self.currentOn = .Right
+      case "None":
+        self.currentOn = .None
+      default:
+        println("we shouldn't have this as an option")
+      }
+    }
+    if let lastOn:String = userDefaults.objectForKey("lastOn") as? String {
+      switch lastOn {
+      case "Left":
+        self.lastOn = .Left
+      case "Right":
+        self.lastOn = .Right
+      case "None":
+        self.lastOn = .None
+      default:
+        println("we shouldn't have this as an option")
+      }
+    }
     
+    // restore timer dates
+    if let leftTimer = userDefaults.objectForKey("leftTimer") as? Timer {
+      timers.left = leftTimer
+    }
+    if let rightTimer = userDefaults.objectForKey("rightTimer") as? Timer {
+      timers.right = rightTimer
+    }
+    if let totalTimer = userDefaults.objectForKey("totalTimer") as? Timer {
+      timers.total = totalTimer
+    }
+    
+  }
+  
+  func persistState() {
+    println("persist/store state")
+    // save button on states
+    userDefaults.setObject(currentOn.rawValue, forKey: "currentOn")
+    // save last button state
+    userDefaults.setObject(lastOn.rawValue, forKey: "lastOn")
+    // save timer dates
+    userDefaults.setObject(timers.left, forKey: "leftTimer")
+    userDefaults.setObject(timers.right, forKey: "rightTimer")
+    userDefaults.setObject(timers.total, forKey: "totalTimer")
   }
   
   func reset() {
