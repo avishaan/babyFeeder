53fe186d058363ab47c547eb0ecaffbace2d234d
Avishaan <avishaan@codeHatcher.com>
2015-09-17 00:53:49 -0400

    basic bar chart with sample data

--- breastFeed/ViewController.swift
+++ breastFeed/ViewController.swift
@@ -16,14 +16,29 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    barChartView.noDataText = "We need data for the bar chart"
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+    // add bar chart data
+    setChart(months, values: unitsSold)
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  func setChart(dataPoints: [String], values: [Double]) {
+    barChartView.noDataText = "You need to provide data for the chart."
+    
+    var dataEntries:[BarChartDataEntry] = []
+    for i in 0..<dataPoints.count {
+      let dataEntry = BarChartDataEntry(value: values[i], xIndex: i)
+      dataEntries.append(dataEntry)
+    }
+    
+    let chartDataSet = BarChartDataSet(yVals: dataEntries, label: "Units Sold")
+    let chartData = BarChartData(xVals: dataPoints, dataSet: chartDataSet)
+    barChartView.data = chartData
+    
+  }
 }
 
